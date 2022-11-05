2fa624580da06bc4f1900e38dac999ea2fb8e7b9
malekhammou <malekhammou2017@gmail.com>
2022-11-05 11:30:49 +0100

    moved params config logic to internal code

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
-echo Reading config parameters...
-ARGS_STRING=$(python3 - << EOF
-import os
-import json
-ARGS_STRING=""
-configFile = open('config.json')
-arguments = json.load(configFile)
-for key,value in arguments.items():
- ARG= f"{key} {value}"
- ARGS_STRING=ARGS_STRING+" "+ARG
-print(ARGS_STRING)
-EOF
-)
 cd code
-python3 create_thumbnail.py $ARGS_STRING ../data/videos
+python3 create_thumbnail.py ../data/videos
