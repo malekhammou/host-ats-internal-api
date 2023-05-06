f506a5edc906524e7fb08536dfc45c5977d3907a
malekhammou <malekhammou2017@gmail.com>
2023-05-06 03:09:37 +0200

    modififed the fromat of the response and added CORS header

--- code/app.py
+++ code/app.py
@@ -1,20 +1,27 @@
 from create_thumbnail import main
-from flask import Flask, request,Response
+from flask import Flask, request,Response,redirect, url_for
 import os
 import requests
 import subprocess
 import m3u8_To_MP4
 import base64
+from flasgger import Swagger,swag_from
+import json
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+swagger = Swagger(app, template_file='docs/createThumbnail.yml')
 
 @app.route('/', methods=['GET'])
-def hello():
-    return "Welcome to HOST-ATS api."
+def index():
+    return redirect(url_for('flasgger.apidocs'))
 
-@app.route('/createThumbnail', methods=['GET', 'POST'])
-def download_video():
+@app.route('/createThumbnail', methods=['POST'])
+@swag_from('docs/createThumbnail.yml')
+
+def generateThumbnailFromM3U8():
     data = request.get_json()
+    print(data)
     video_url = data['video_url']
     video_filename = data['video_filename'] + '.mp4'
     m3u8_To_MP4.multithread_download(video_url)
@@ -30,29 +37,21 @@ def download_video():
     if not image_files:
         return Response('No images found', status=404)
     else:
-        boundary = 'image_boundary'
         image_data = []
         for image_file in image_files:
             with open(os.path.join(output_folder, image_file), 'rb') as f:
                 image_bytes = f.read()
-            image_data.append((
-                '--' + boundary,
-                'Content-Disposition: form-data; name="image"; filename="' + image_file + '"',
-                'Content-Type: image/jpeg',
-                '',
-                base64.b64encode(image_bytes).decode('utf-8'),
-                ''
-            ))
-        response_data = []
-        for item in image_data:
-            response_data.extend([bytes(line + '\r\n', 'utf-8') for line in item])
-        response_data.append(bytes('--' + boundary + '--\r\n', 'utf-8'))
-        response_headers = {
-            'Content-Type': 'multipart/form-data; boundary=' + boundary,
-            'Content-Length': str(sum(len(line) for line in response_data))
-        }
-        return Response(b''.join(response_data), headers=response_headers)
-
+            image_data.append({
+                'name': image_file,
+                'content-type': 'image/jpeg',
+                'base64': base64.b64encode(image_bytes).decode('utf-8')
+            })
+        response_data = {'images': image_data}
+        response = Response(json.dumps(response_data), mimetype='application/json')
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
 
