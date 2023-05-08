f64031786f6fbd8cc5e4ce7e1ee8d6f1fc6cb5bc
malekhammou <malekhammou2017@gmail.com>
2023-05-08 12:59:47 +0200

    added download and execution time to the response

--- code/app.py
+++ code/app.py
@@ -1,13 +1,12 @@
 from create_thumbnail import main
-from flask import Flask, request,Response,redirect, url_for
+from flask import Flask, request,Response
 import os
-import requests
-import subprocess
 import m3u8_To_MP4
 import base64
 #from flasgger import Swagger,swag_from
 import json
-from flask_cors import CORS, cross_origin
+import time
+
 
 app = Flask(__name__)
 #swagger = Swagger(app, template_file='docs/createThumbnail.yml')
@@ -25,13 +24,19 @@ def generateThumbnailFromM3U8():
     print(data)
     video_url = data['video_url']
     video_filename = data['video_filename'] + '.mp4'
+    start_time = time.time()
     m3u8_To_MP4.multithread_download(video_url)
+    download_time = time.time() - start_time
     os.rename('m3u8_To_MP4.mp4', video_filename)
     input_folder = os.path.join(os.path.dirname(os.getcwd()), 'input')
     os.makedirs(input_folder, exist_ok=True)
     os.replace(video_filename, os.path.join(input_folder, video_filename))
+    start_time = time.time()
     main()
-    # Get images from output folder and send as multipart response
+    generate_thumbnail_time = time.time() - start_time
+
+    print("Download time",download_time)
+    print("Thumbnail generation time",generate_thumbnail_time)
     output_folder = os.path.join(os.path.dirname(os.getcwd()), 'output', video_filename)
 
     image_files = [f for f in os.listdir(output_folder) if f.endswith('.jpg') or f.endswith('.png')]
@@ -47,7 +52,9 @@ def generateThumbnailFromM3U8():
                 'content-type': 'image/jpeg',
                 'base64': base64.b64encode(image_bytes).decode('utf-8')
             })
-        response_data = {'images': image_data}
+        response_data = {'images': image_data,
+                         'downloadTime':download_time,
+                         'thumbnailGenerationTime':generate_thumbnail_time}
         response = Response(json.dumps(response_data), mimetype='application/json')
         response.headers.add('Access-Control-Allow-Origin', '*')
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
